515fc3f8d7564f194c6fbcfb6de4c24f5964b047
antony <tehanovanton@gmail.com>
2021-09-17 23:42:13 +0300

    task 1 done

--- 03-01.js
+++ 03-01.js
@@ -1,14 +1,67 @@
 var http = require('http')
 var fs = require('fs')
+var readLine = require('readline')
 var port = 5000
 var getCurrentAppStateUrl = 'get_current_app_state'
-var appStates = ['norm', 'stop', 'test', 'idle']
+var appStates = ['norm', 'test', 'idle']
+var appCommands = ['getState', 'stop']
 var textPlainHeaderOptions = { 'Content-Type': 'text/plain' }
 var textHtmlHeaderOptions = { 'Content-Type': 'text/html; charset=utf-8' }
 var htmlPath = './03_01.html'
 var currentAppState = appStates[0]
 
-http.createServer(function(request, response) {
+function isCommand(inData) {
+    return appCommands.includes(inData)
+}
+
+function isState(inData) {
+    return appStates.includes(inData)
+}
+
+function executeCommand(inData) {
+    if (inData == appCommands[0]) {
+        console.log(`app: ${currentAppState}`)
+    }
+    else if (inData == appCommands[1]) {
+        return appCommands[1]
+    }
+
+    return 'run'
+}
+
+function changeState(inData) {
+    currentAppState = inData
+    console.log('state changed to: ' + currentAppState)
+}
+
+commandLine = () => {
+    var rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    rl.question('command/state: ', inData => {             
+        rl.close()
+        var will_run = 'run'
+
+        if (isCommand(inData)) {
+            will_run = executeCommand(inData)
+        }
+        else if (isState(inData))
+            changeState(inData)
+        else {
+            commandLine()
+            return
+        }
+
+        if (will_run == 'run')
+            commandLine()
+        else
+            console.log('Goodbye console')
+    })
+} 
+
+server = http.createServer(function(request, response) {
     var headerOptions = null
     var responseEnd = null
 
@@ -25,6 +78,8 @@ http.createServer(function(request, response) {
 
     response.writeHeader(200, headerOptions)
     response.end(responseEnd)
-}).listen(port)
+})
+server.listen(port)
 
 console.log(`Server running at http//localhost:${port}`)
+commandLine()
