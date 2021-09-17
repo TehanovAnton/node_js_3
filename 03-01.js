0370dcb3a92d772d2e9b2ef3e8b0d99672ddea87
antony <tehanovanton@gmail.com>
2021-09-17 21:47:29 +0300

    task_1, return app state

---
+++ 03-01.js
@@ -0,0 +1,28 @@
+var http = require('http')
+var fs = require('fs')
+var port = 5000
+var getCurrentAppStateUrl = 'get_current_app_state'
+var appStates = ['norm', 'stop', 'test', 'idle']
+var textPlainHeaderOptions = { 'Content-Type': 'text/plain' }
+var textHtmlHeaderOptions = { 'Content-Type': 'text/html; charset=utf-8' }
+var htmlPath = './03_01.html'
+var currentAppState = appStates[0]
+
+http.createServer(function(request, response) {
+    var headerOptions = null
+    var responseEnd = null
+
+    if (request.url.endsWith(getCurrentAppStateUrl)) {
+        headerOptions = textPlainHeaderOptions
+        responseEnd = currentAppState
+    }
+    else {
+        headerOptions = textHtmlHeaderOptions
+        responseEnd = fs.readFileSync(htmlPath)        
+    }
+
+    response.writeHeader(200, headerOptions)
+    response.end(responseEnd)
+}).listen(port)
+
+console.log(`Server running at http//localhost:${port}`)
